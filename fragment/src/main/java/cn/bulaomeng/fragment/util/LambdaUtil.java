e9a594ca526919e2fa9c53cfaea17d5d91d39f86
tjy <tjy@weds.com.cn>
2020-03-09 16:39:24 +0800

    添加lambda 表达式去重

---
+++ fragment/src/main/java/cn/bulaomeng/fragment/util/LambdaUtil.java
@@ -0,0 +1,15 @@
+package cn.bulaomeng.fragment.util;
+
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class LambdaUtil {
+
+    private static <T> Predicate<T> distinctByKey(Function<? super T, ?> keyExtractor) {
+        Set<Object> seen = ConcurrentHashMap.newKeySet();
+        return t -> seen.add(keyExtractor.apply(t));
+    }
+
+}
